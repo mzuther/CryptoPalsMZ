5e46c82e9f287a77b4859eeef57fb10bfde4ce47
Martin Zuther <code@mzuther.de>
2026-02-16 20:15:02 +0100

    factor out constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,7 @@
 use std::ops::Range;
 use std::collections::HashMap;
 
-const ENGLISH_LETTER_FREQUENCIES: [(char, f64); 27] = [
-    (' ', 0.250),
-    ('e', 0.127),
-    ('t', 0.091),
-    ('a', 0.082),
-    ('o', 0.075),
-    ('i', 0.070),
-    ('n', 0.067),
-    ('s', 0.063),
-    ('h', 0.061),
-    ('r', 0.060),
-    ('d', 0.043),
-    ('l', 0.040),
-    ('c', 0.028),
-    ('u', 0.028),
-    ('m', 0.024),
-    ('w', 0.024),
-    ('f', 0.022),
-    ('g', 0.020),
-    ('y', 0.020),
-    ('p', 0.019),
-    ('b', 0.015),
-    ('v', 0.0098),
-    ('k', 0.0077),
-    ('j', 0.0016),
-    ('x', 0.0015),
-    ('q', 0.0012),
-    ('z', 0.0007),
-];
+pub mod constants;
 
 pub fn fixed_xor(bytes_input: &Vec<u8>, bytes_key: &Vec<u8>) -> Vec<u8> {
     let mut bytes_xor = Vec::new();
@@ -190,13 +162,7 @@ fn score_letter_frequencies(bytes_input: &Vec<u8>) -> f64 {
         *count += percent_per_letter;
     }
 
-    let mut english_letter_frequencies = HashMap::new();
-
-    for letter in ENGLISH_LETTER_FREQUENCIES {
-        let key = letter.0 as u8;
-        english_letter_frequencies.insert(key, letter.1);
-    }
-
+    let english_letter_frequencies = crate::constants::get_english_letter_frequencies();
     let mut score = 0.0;
 
     // bonus for letters matching expected frequency
